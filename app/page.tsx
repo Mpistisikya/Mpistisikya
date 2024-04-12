cdeec1be18a2df950a5765b3644ac20920852c9c
Mpistisikya <166576953+Mpistisikya@users.noreply.github.com>
2024-04-12 17:45:06 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -109,9 +109,11 @@ export default function Home() {
       </div>
   
     </main>
-    <!-- Calendly inline widget begin -->
+  
+  );
+   <!-- Calendly inline widget begin -->
 <div class="calendly-inline-widget" data-url="https://calendly.com/mpistisikya" style="min-width:320px;height:700px;"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
 <!-- Calendly inline widget end -->
-  );
 }
+
