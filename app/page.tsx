0e83609626e220ddd0c06561cce5ce57289d41d7
Mpistisikya <166576953+Mpistisikya@users.noreply.github.com>
2024-04-15 01:51:52 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -47,39 +47,7 @@ export default function Home() {
 
     <section id="about" className="section about">
 
-          <div className="container row gy-3 col-lg-6">
-            <Image src="aboutimg.svg" alt="" class="img-fluid">
-          </div>
-
-          <div class="container row gy-3 col-lg-6 d-flex flex-column justify-content-center about-content ps-0 ps-lg-3">
-              <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-              <p class="fst-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
-              </p>
-              <ul>
-                <li>
-                  <i class="bi bi-diagram-3"></i>
-                  
-                    <h4>Ullamco laboris nisi ut aliquip consequat</h4>
-                    <p>Magni facilis facilis repellendus cum excepturi quaerat praesentium libre trade</p>
-                  
-                </li>
-                <li>
-                  <i class="bi bi-fullscreen-exit"></i>
-                  
-                    <h4>Magnam soluta odio exercitationem reprehenderi</h4>
-                    <p>Quo totam dolorum at pariatur aut distinctio dolorum laudantium illo direna pasata redi</p>
-                  
-                </li>
-              </ul>
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum
-              </p>
-            </div>
-
+     
           
       
 
